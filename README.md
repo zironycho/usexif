36edf2501098a316b1217de1a0003705f5747a01
Juncheol Cho <zironycho@gmail.com>
2017-02-26 14:48:33 +0900

    update setup environment

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # usexif
+Geographic Coordinate(longitude, latitude, and altitude) from raw EXIF data
+
+## Install
+```
+pip install usexif
+```
+
+## Example
+```
+import usexif
+ret = usexif.fromfile('image/path/here.jpg')
+
+ret['longitude']
+# 126.16250277777779
+
+ret['latitude']
+# 34.36355833333334
+
+ret['altitude']
+# 3.0
+
+ret['taken_date']
+# datetime.datetime(2016, 4, 18, 10, 41, 6)
+```
+
+## used parser
+```
+exif-py
+```
--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from distutils.core import setup
+import pypandoc
+import os
+
+
+readme_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
+long_desc = pypandoc.convert('README.md', 'rst')
+version='1.0.0'
 
 setup(
     name='usexif',
     packages=['usexif'],
-    version='1.0',
+    version=version,
     description='get geo data and taken date',
+    long_description=long_desc,
     author='Juncheol Cho',
     author_email='zironycho@gmail.com',
     url='https://github.com/zironycho/usexif',
-    download_url='https://github.com/zironycho/usexif/tarball/1.0',
+    download_url='https://github.com/zironycho/usexif/tarball/{}'.format(version),
     keywords=['exif', 'geo', 'taken', 'geo', 'geographic coordinate', 'coordinate'],
     license='MIT',
     classifiers=[],
+    install_requires=[
+       'exifread>=2.1.2'
+    ],
 )
